8f932f084a548f43fae966f9dee552ed318727e5
Jennifer Parra <jparra.biblio@gmail.com>
2018-05-22 12:04:51 -0400

    Agregué códigos que me faltaban

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,15 @@
 window.onload = function () {
-//crear variable para boton - Estas son variables globales
     var btn = document.getElementById('button-change');
     var txt = document.getElementById('paragraph');
     var btnDos = document.getElementById('magic');
     var txtDos = document.getElementById('text');
-
-// ejecutar sobre variable con addEventListener
-    btn.addEventListener('click', function(){
-        txt.innerHTML = '<h2>Git & Github TE AMAMOS</h2>';
+  
+    btn.addEventListener('click', function() {
+      txt.innerHTML = '<h2>Git & Github TE AMAMOS</h2>';
     })
-
-    btn.addEventListener('click', function(){
-        //text.innerHTML = ''
-        txtDos.classList.add('disappear');
-
+  
+    btnDos.addEventListener('click', function() {
+      //txtDos.innerHTML = '';
+      txtDos.classList.add('disappear');
     })
-}
+  }
